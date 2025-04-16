d890565855f16934d4afc35829f28cd1c5afb119
monogramatico123 <alexb.inf.4891@gmail.com>
2025-04-16 16:49:33 -0500

    Initial commit

---
+++ R/manejar_excel.R
@@ -0,0 +1,23 @@
+#' Función que devuelva todas las hojas y dentro de las hojas las columnas en listas anidadas
+#'
+#' @param ruta Ruta del archivo excel
+#'
+#' @returns Es un evector con las hojas del excel
+#' @export
+#'
+#' @examples
+#' hojas_excel(NULL)
+#' @import readxl
+#' @import dplyr
+#' @import purrr
+#' @import RSQLite
+hojas_excel <- function(ruta){
+
+  if(!is.null(ruta)){
+    hojas <- readxl::excel_sheet(ruta)
+
+    hojas %>%
+      purrr::map(~read_excel(ruta,sheet = .x) %>% names())
+
+  }
+}
